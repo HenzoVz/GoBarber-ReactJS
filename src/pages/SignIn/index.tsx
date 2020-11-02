c7b1024d47f79e006900aa2639bfe7163e6c626e
henzovz <murilohezo@gmail.com>
2020-11-02 11:29:53 -0300

    add toast context

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -22,9 +23,8 @@ interface SigInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useAuth();
-
-  console.log(user)
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(async (data: SigInFormData) => {
     try {
@@ -43,16 +43,19 @@ const SignIn: React.FC = () => {
         abortEarly: false
       });
 
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password
       });
     } catch (error) {
-      const errors = getValidationErrors(error);
+      if(error instanceof Yup.ValidationError) {
 
-      formRef.current?.setErrors(errors);
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+      }
+      addToast();
     }
-  }, [signIn]);
+  }, [signIn, addToast]);
 
 
   return (
